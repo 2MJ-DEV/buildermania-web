f3941c37c35fd646e514a2e77ea011942fc34875
2MJ-DEV <julesmukadi.dev@gmail.com>
2025-11-25 16:56:14 +0200

    Correction de Pricing

---
+++ app/pricing/page.tsx
@@ -0,0 +1,11 @@
+export default function PricingPage() {
+  return (
+    <div>
+      <h1>Pricing Page</h1>
+      <p>
+        Welcome to the pricing page. Here you can find information about our
+        pricing plans.
+      </p>
+    </div>
+  );
+}

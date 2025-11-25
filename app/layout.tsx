d4b921858a0a5c94b222dc0f60be68dbba7755e6
2MJ-DEV <julesmukadi.dev@gmail.com>
2025-11-25 17:22:16 +0200

    Creation d'Informations sur la vitesse

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Bienvenue dans BuilderMania",
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main className="w-[80vw] mx-auto">
           {children}
           <Analytics />
+          <SpeedInsights />
           </main>
         <Footer />
       </body>
